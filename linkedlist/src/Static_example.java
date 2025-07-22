0d016b9b6434cc960c8e8ac3f167ca1244d14462
abi <abikrishnaviswanathan@gmail.com>
2025-07-22 16:32:34 +0530

    student details

---
+++ linkedlist/src/Static_example.java
@@ -0,0 +1,18 @@
+
+public class Static_example {
+
+    static int n = 10;
+    public static void main(String[] args)
+    {
+
+        ret();
+        System.out.println(n);
+
+    }
+
+    public static void ret () {
+
+        n++;
+
+    }
+}
